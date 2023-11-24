0bc2beb4d594bbeeae33d9bb9ceddf94bc8d41bd
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-11-24 10:31:39 +0600

    basic server set up

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000;
+
+// Pre-installed middlewares
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// Custom middlewares
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+// Mongoose connection
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Mongoose Schema and Model for users 
+const userSchema = new mongoose.Schema({
+  email: String,
+  displayName: String,
+  photoURL: String,
+  role: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const usersModel = mongoose.model('User', userSchema);
+
+// Auth related API
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  console.log('I need a new jwt', user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '365d',
+  });
+  res
+    .cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    })
+    .send({ success: true });
+});
+
+// Logout
+app.get('/logout', async (req, res) => {
+  try {
+    res
+      .clearCookie('token', {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true });
+    console.log('Logout successful');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Save or modify user email, status in DB
+app.put('/users/:email', async (req, res) => {
+  const email = req.params.email;
+  const user = req.body;
+  const query = { email: email };
+  const options = { upsert: true };
+  const isExist = await usersModel.findOne(query);
+  console.log('User found?----->', isExist);
+  if (isExist) return res.send(isExist);
+  const result = await usersModel.updateOne(
+    query,
+    {
+      $set: { ...user, timestamp: Date.now() },
+    },
+    options
+  );
+  res.send(result);
+});
+
+// Send a ping to confirm a successful connection
+await mongoose.connection.db.admin().ping();
+console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Health Hub Server..');
+});
+
+app.listen(port, () => {
+  console.log(`Health Hub is running on port ${port}`);
+});
